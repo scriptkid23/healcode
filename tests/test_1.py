30421904d37ba6bcd20ecb906842a355f7755766
duongess <duongcantat@gmail.com>
2025-08-18 07:45:41 +0000

    fix test file

--- tests/test_1.py
+++ tests/test_1.py
@@ -1,5 +1,9 @@
 import asyncio
-from ai.example import main
+from ai.example import main as ai_main
+from editor.example_usage import main as editor_main
 
 def test_ai():
-    asyncio.run(main())
+    asyncio.run(ai_main())
+
+def test_editor():
+    editor_main()
